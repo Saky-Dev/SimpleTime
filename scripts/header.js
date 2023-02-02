461511cbc0302fe7a8f92fa148c91b4327df0b66
IsaacGH <garciaisaac468@gmail.com>
2023-02-02 12:51:23 -0600

    Monospace font was added to clock; Syntax errors were fized in header js; Main clock functionality was added now the clock is working with js code

--- scripts/header.js
+++ scripts/header.js
@@ -1,12 +1,12 @@
-navbarVisivility = false
+let navbar_visivility = false
 
-btn_hamburguer_menu = document.querySelector('button#hamburguer_menu')
+let btn_hamburguer_menu = document.querySelector('button#hamburguer_menu')
 
 /* Code to change the visibility of navbar in
  * portrait mode and change the class of the button
  * of the menu
  */
 btn_hamburguer_menu.addEventListener('click', e => {
-  navbarVisivility = !navbarVisivility
-  btn_hamburguer_menu.className = `${navbarVisivility ? 'visible' : 'hidden'}`
+  navbar_visivility = !navbar_visivility
+  btn_hamburguer_menu.className = `${navbar_visivility ? 'visible' : 'hidden'}`
 })
--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,37 @@
-twelveHoursFormat = false
+let twelve_hours_format = false
+let time_increment = undefined
+let time = new Date()
 
-btn_switch = document.querySelector('button#switch')
-spn_midday = document.querySelector('span#midday')
+let spn_clock = document.querySelector('span#clock')
+let btn_switch = document.querySelector('button#switch')
+let spn_midday = document.querySelector('span#midday')
+
+const func_get_now = () => {
+  let hrs = twelve_hours_format ? time.getHours() - 12 : time.getHours()
+  let min = `${time.getMinutes() < 10 ? '0' : ''}${time.getMinutes()}`
+  let sec = `${time.getSeconds() < 10 ? '0' : ''}${time.getSeconds()}`
+
+  hrs = `${hrs < 10 ? '0' : ''}${hrs}`
+
+  return `${hrs}:${min}:${sec}`
+}
+
+const func_update_time = () => spn_clock.innerHTML = func_get_now()
+
+time_increment = setInterval(() => {
+  time.setTime(time.getTime() + 1000)
+  func_update_time()
+}, 1000)
 
 btn_switch.addEventListener('click', () => {
-  twelveHoursFormat = !twelveHoursFormat
+  twelve_hours_format = !twelve_hours_format
 
-  btn_switch.className = twelveHoursFormat ? 'on' : 'off'
-  spn_midday.className = twelveHoursFormat ? 'visible' : 'hidden'
+  btn_switch.className = twelve_hours_format ? 'on' : 'off'
+  spn_midday.className = twelve_hours_format ? 'visible' : 'hidden'
+
+  func_update_time()
 })
+
+;(() => {
+  func_update_time()
+})()
